f2eaa2fab5fe353d0b355e71c244d94db2ac4bb2
Karl Speer <kspeer@qualtrax.com>
2014-06-13 16:38:14 -0400

    Beginning implementation of statsd

--- MomentOfZenGenerator.Web/MomentOfZenGenerator.Web/Controllers/GeneratorController.cs
+++ MomentOfZenGenerator.Web/MomentOfZenGenerator.Web/Controllers/GeneratorController.cs
@@ -1,13 +1,17 @@
-﻿using MomentOfZenGenerator.Wordnik;
-using MomentOfZenGenerator.YouTube;
-using System;
+﻿using System;
+using System.Diagnostics;
 using System.Web.Mvc;
+using MomentOfZenGenerator.Wordnik;
+using MomentOfZenGenerator.YouTube;
+using statsd_csharp;
 
 namespace MomentOfZenGenerator.Web.Controllers
 {
     public class GeneratorController : Controller
     {
         private Generator generator;
+        private IStatsdClient statsdClient;
+        private Stopwatch stopwatch;
 
         public GeneratorController()
         {
@@ -21,16 +25,26 @@ namespace MomentOfZenGenerator.Web.Controllers
             var filter = new Filter(youTubeResponseProvider);
 
             generator = new Generator(random, wordnikResponseProvider, filter);
+
+            statsdClient = StatsdClient.Create("191.238.44.201", 8125, "moment-of-zen-generator");
+            stopwatch = new Stopwatch();
         }
 
         public ActionResult Generate()
         {
+            stopwatch.Start();
+
             var videoUrl = generator.GetMomentOfZen();
 
             while (String.IsNullOrEmpty(videoUrl))
                 videoUrl = generator.GetMomentOfZen();
 
             ViewBag.VideoUrl = videoUrl;
+
+            var totalMilliseconds = Convert.ToInt32(stopwatch.Elapsed.TotalMilliseconds);
+            statsdClient.SendTiming("generator.duration", totalMilliseconds);
+
+            stopwatch.Reset();
 
             return View();
         }
